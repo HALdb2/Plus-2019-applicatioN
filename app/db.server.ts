c15c1a6ff7f7afee1ceabdfd7ec3e3a8c9889e67
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-03-11 11:14:30 +0000

    Adding package info to 'build-info/dotnet/corefx/release/1.0.0/Latest_Packages.txt' for stable

--- app/db.server.ts
+++ app/db.server.ts
@@ -8,4 +8,5 @@ declare global {
 
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every ch
+// create a new connection to the DB with every change either.
+// in production we'll have a single connect
