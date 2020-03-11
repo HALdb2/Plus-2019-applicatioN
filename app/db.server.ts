7831b44061674247f6ea1348844d59a8396a8071
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-03-11 05:56:16 +0000

    Cleanup changelog

--- app/db.server.ts
+++ app/db.server.ts
@@ -8,4 +8,4 @@ declare global {
 
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
-// cre
+// create a new connection to the DB with every ch
