f7a9933699771589c8e7396a0e7ba3c874555389
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-04-25 05:54:44 +0000

    other files

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -11,4 +11,5 @@ export async function getUserByEmail(email: string) {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function createUser(
+export async function createUser(email: string, password: string) {
+  const hashedPasswo
