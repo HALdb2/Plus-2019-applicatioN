843f5b0dd2d4711e8db8f0089b46e0bff7a9434c
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-04-25 13:21:34 +0000

    clean debug

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -13,4 +13,8 @@ export async function getUserByEmail(email: string) {
 
 export async function createUser(email: string, password: string) {
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await prisma.user.cr
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+    
