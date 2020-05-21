14042b9e6a33a95e5364f252c2bc8d2e103f4a65
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-05-21 18:17:03 +0000

    Linux 3.4.61

--- app/root.tsx
+++ app/root.tsx
@@ -12,4 +12,5 @@ import type { LinksFunction, MetaFunction, LoaderFunction } from "remix";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
 
-export const links: LinksFunction = () =>
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesh
