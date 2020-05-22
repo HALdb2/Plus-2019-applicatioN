554a9e6748790ba72d8d7197ff0d02ec21a6dc43
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-05-22 10:11:01 +0000

    some minor fixes

--- app/root.tsx
+++ app/root.tsx
@@ -19,4 +19,8 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Remix Notes",
-  viewport: "width=device-width
+  viewport: "width=device-width,initial-scale=1",
+});
+
+type LoaderData = {
+  user: Awa
