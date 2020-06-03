d86783071199f5e6fcbdeed4fd190aba627e3fbf
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-06-03 21:09:02 +0000

    Update 1-bundesliga-i.txt

--- app/root.tsx
+++ app/root.tsx
@@ -36,4 +36,7 @@ export default function App() {
   return (
     <html lang="en" className="h-full">
       <head>
-        <Met
+        <Meta />
+        <Links />
+      </head>
+      <body classN
