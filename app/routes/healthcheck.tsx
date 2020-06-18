f54f8462729ebf0731c34386574eaa786ee80eb3
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-06-18 07:16:14 +0000

    ipv6/addrlabel: fix ip6addrlbl_get()

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,3 +1,5 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunction } from "remix";
-import { prisma } from "~/db
+import { prisma } from "~/db.server";
+
+export const loader: LoaderFunction = async (
