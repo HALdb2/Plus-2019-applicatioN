c6bf1fd0badcd459243dac3753bf956a78dbd5da
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-07-29 07:31:53 +0000

    Cleanup formatting

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,4 +14,4 @@ export default function Index() {
                 src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
                 alt="Sonic Youth On Stage"
               />
-              <div className="abso
+              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-mu
