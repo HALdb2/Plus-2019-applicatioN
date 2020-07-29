6601cf336d41dde8ecbbe0f8473b96f13272644b
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-07-29 11:27:54 +0000

    Fixes syntax error

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,4 +14,6 @@ export default function Index() {
                 src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
                 alt="Sonic Youth On Stage"
               />
-              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-mu
+              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+            </div>
+            <div className="lg:p
