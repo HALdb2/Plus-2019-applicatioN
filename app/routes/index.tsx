d0bb5e4af2ff9bad87d718c2d517406ae56f7eca
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-08-19 19:46:01 +0000

    Changed version

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -26,4 +26,5 @@ export default function Index() {
                 Check the README.md file for instructions on how to get this
                 project deployed.
               </p>
-              <div className="mx-auto mt-10 max-w-sm
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+              
