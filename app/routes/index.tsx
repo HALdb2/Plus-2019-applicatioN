e9ee5ed4003dcb3e93f744f431e98ca8d44d4e57
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-08-21 12:33:57 +0000

    add init file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -27,4 +27,7 @@ export default function Index() {
                 project deployed.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-              
+                {user ? (
+                  <Link
+                    to="/notes"
+ 
