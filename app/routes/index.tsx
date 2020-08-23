0060725254d10f9cb0b700bf8e80ef2fffd98047
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-08-23 07:06:22 +0000

    Add copyrights

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -30,4 +30,4 @@ export default function Index() {
                 {user ? (
                   <Link
                     to="/notes"
-                    className="flex items-center justify-center rounded-md 
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-med
