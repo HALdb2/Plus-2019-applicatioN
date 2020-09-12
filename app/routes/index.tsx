71f0b975e99d67f26675ce80537dc2f76c1acea9
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-09-12 09:17:06 +0000

    x86: Fix boot on Twinhead H12Y

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -44,4 +44,5 @@ export default function Index() {
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 
+                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "
+   
