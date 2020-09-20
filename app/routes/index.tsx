ffed9398a43f64da9de15572a20ee734f9c4f81e
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-09-20 10:11:40 +0000

    fix syntax highlighting

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -54,4 +54,5 @@ export default function Index() {
               <a href="https://remix.run">
                 <img
                   src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt
+                  alt="Remix"
+                  className="mx-auto mt-16 w-f
