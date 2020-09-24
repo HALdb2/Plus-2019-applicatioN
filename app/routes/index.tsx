8f2e1bae4fdc7d952d596a3a6197059b4041ff35
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-09-24 15:45:30 +0000

    [l10n] Updated Italian translation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -63,4 +63,6 @@ export default function Index() {
         </div>
 
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-
+          <div className="mt-6 flex flex-wrap justify-center gap-8">
+            {[
+         
