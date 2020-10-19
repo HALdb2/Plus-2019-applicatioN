ad2e0099d999aab6c4e37c79e392081521a00eb9
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-10-19 12:11:21 +0000

    Forward port zendframework/zf2#3648

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -72,4 +72,5 @@ export default function Index() {
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/158238105-e7279a0c-1640-40db-86b0-3d3a10aab824.svg",
-                alt: "Post
+                alt: "PostgreSQL",
+                href: "https://www.postgresql.org
