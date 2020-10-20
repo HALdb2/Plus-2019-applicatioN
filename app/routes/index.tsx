e601c97ac27838ddec794fec0367da6995c4d227
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-10-20 13:20:21 +0000

    add NNLS

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -76,4 +76,5 @@ export default function Index() {
                 href: "https://www.postgresql.org/",
               },
               {
-                src: "https://user-images.githubusercontent.com/150068
+                src: "https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg",
+   
