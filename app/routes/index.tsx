fa207747425124dae598a10bfed2af7dc110198d
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2020-11-02 08:49:22 +0000

    Fixed lint

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -91,4 +91,4 @@ export default function Index() {
                 href: "https://www.cypress.io",
               },
               {
-                src: "https://user-images.github
+                src: "https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af
