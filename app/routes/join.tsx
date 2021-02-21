c4824265ff94be3dab0813ed8c845cc9f55c98fa
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-02-21 05:29:56 +0000

    Greek translation

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -39,3 +39,52 @@ export const action: ActionFunction = async ({ request }) => {
       { errors: { email: "Email is invalid" } },
       { status: 400 }
     );
+  }
+
+  if (typeof password !== "string") {
+    return json<ActionData>(
+      { errors: { password: "Password is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (password.length < 8) {
+    return json<ActionData>(
+      { errors: { password: "Password is too short" } },
+      { status: 400 }
+    );
+  }
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return json<ActionData>(
+      { errors: { email: "A user already exists with this email" } },
+      { status: 400 }
+    );
+  }
+
+  const user = await createUser(email, password);
+
+  return createUserSession({
+    request,
+    userId: user.id,
+    remember: false,
+    redirectTo: typeof redirectTo === "string" ? redirectTo : "/",
+  });
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Sign Up",
+  };
+};
+
+export default function Join() {
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") ?? undefined;
+  const actionData = useActionData() as ActionData;
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (actionData?.errors?.email) {
