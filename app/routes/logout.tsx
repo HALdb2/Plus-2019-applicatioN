4db7e37a2373e9f3c48970621cb97527846a089c
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-03-26 06:20:27 +0000

    markdown format

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -3,4 +3,8 @@ import { redirect } from "remix";
 import { logout } from "~/session.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  return logout(request
+  return logout(request);
+};
+
+export const loader: LoaderFunction = async () => {
+  retu
