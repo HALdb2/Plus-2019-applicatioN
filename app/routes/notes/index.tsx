45f4f90a0f186ec2e9beb1dcad2120c7d8a51fbb
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-03-31 08:15:15 +0000

    kernel: add support for gcc 5

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -4,4 +4,6 @@ export default function NoteIndexPage() {
   return (
     <p>
       No note selected. Select a note on the left, or{" "}
-      <Link to="new"
+      <Link to="new" className="text-blue-500 underline">
+        create a new note.
+    
