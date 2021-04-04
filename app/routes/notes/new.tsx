1a5dcb439cfee39f4aded050bad555eeab345b08
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-04-04 12:11:33 +0000

    Add test

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -33,3 +33,73 @@ export const action: ActionFunction = async ({ request }) => {
       { errors: { body: "Body is required" } },
       { status: 400 }
     );
+  }
+
+  const note = await createNote({ title, body, userId });
+
+  return redirect(`/notes/${note.id}`);
+};
+
+export default function NewNotePage() {
+  const actionData = useActionData() as ActionData;
+  const titleRef = React.useRef<HTMLInputElement>(null);
+  const bodyRef = React.useRef<HTMLTextAreaElement>(null);
+
+  React.useEffect(() => {
+    if (actionData?.errors?.title) {
+      titleRef.current?.focus();
+    } else if (actionData?.errors?.body) {
+      bodyRef.current?.focus();
+    }
+  }, [actionData]);
+
+  return (
+    <Form
+      method="post"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 8,
+        width: "100%",
+      }}
+    >
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Title: </span>
+          <input
+            ref={titleRef}
+            name="title"
+            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            aria-invalid={actionData?.errors?.title ? true : undefined}
+            aria-errormessage={
+              actionData?.errors?.title ? "title-error" : undefined
+            }
+          />
+        </label>
+        {actionData?.errors?.title && (
+          <Alert className="pt-1 text-red-700" id="title=error">
+            {actionData.errors.title}
+          </Alert>
+        )}
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Body: </span>
+          <textarea
+            ref={bodyRef}
+            name="body"
+            rows={8}
+            className="w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-6"
+            aria-invalid={actionData?.errors?.body ? true : undefined}
+            aria-errormessage={
+              actionData?.errors?.body ? "body-error" : undefined
+            }
+          />
+        </label>
+        {actionData?.errors?.body && (
+          <Alert className="pt-1 text-red-700" id="body=error">
+            {actionData.errors.body}
+          </Alert>
+        )}
+      </div>
