4c6038d3cdaadd785d4a1c93457f7665efcbe6b1
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-04-11 11:57:02 +0000

    fixed readme typos

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,2 +1,5 @@
 import { Form, json, useLoaderData, Outlet, Link, NavLink } from "remix";
-import type { LoaderFunction } from "r
+import type { LoaderFunction } from "remix";
+
+import { requireUserId } from "~/session.server";
+import { useU
