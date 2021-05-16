ab2ca89a45feaff6bcba9361a631703a64969af1
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-05-16 17:56:13 +0000

    zram: don't grab mutex in zram_slot_free_noity

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -32,4 +32,8 @@ export default function NotesPage() {
             className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
           >
             Logout
-          </
+          </button>
+        </Form>
+      </header>
+
+      <main className
