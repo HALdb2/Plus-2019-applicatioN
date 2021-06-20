eda15e66ec6d74a01d38fc1a700e250a50a7e1aa
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-06-20 14:26:25 +0000

    cleanup comment

--- app/session.server.ts
+++ app/session.server.ts
@@ -2,4 +2,6 @@ import { createCookieSessionStorage, redirect } from "remix";
 import invariant from "tiny-invariant";
 
 import type { User } from "~/models/user.server";
-import { getUserById } from "~/models/user.serv
+import { getUserById } from "~/models/user.server";
+
+invariant(process.env.SESSION_SECRET, "SESS
