e663e32e24b24905576ecf5a9bca956b0a36f054
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-06-20 22:38:43 +0000

    removed lib

--- app/session.server.ts
+++ app/session.server.ts
@@ -6,4 +6,7 @@ import { getUserById } from "~/models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
-export const sessionStorage = cre
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: t
