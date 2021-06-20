0284f761e7944f9429a6271bbf302dd4054ea677
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-06-20 09:27:06 +0000

    Fix some clippy warnings

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,4 +1,5 @@
 import { createCookieSessionStorage, redirect } from "remix";
 import invariant from "tiny-invariant";
 
-import type { User } from "
+import type { User } from "~/models/user.server";
+import { getUserById } from "~/models/user.serv
