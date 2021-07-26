6fac4d21b92731475eda4ce0a1bc6347309083ef
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-07-26 11:06:07 +0000

    Update github-markup requirement from ~> 1.3 to ~> 3.0

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,5 @@
 import { validateEmail } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
-  expect(vali
+  expect(validateEmail(undefined)).toBe(false);
+  expect(validateEmail(
