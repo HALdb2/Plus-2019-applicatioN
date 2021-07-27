d88bb0c9f08d7b573ed67a5d319fe8a18aaacba1
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-07-27 09:07:27 +0000

    Updated POT file.

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -2,4 +2,5 @@ import { validateEmail } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
-  expect(validateEmail(
+  expect(validateEmail(null)).toBe(false);
+  expect(validateEmail("")).toBe(false);
