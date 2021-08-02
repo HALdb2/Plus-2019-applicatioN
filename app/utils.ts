cf257871517aa24556ebe01b4f60c586d67fc7b7
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-08-02 08:53:08 +0000

    DOC: release notes

--- app/utils.ts
+++ app/utils.ts
@@ -1,4 +1,7 @@
 import { useMemo } from "react";
 import { useMatches } from "remix";
 
-import type { User } from "~/models/
+import type { User } from "~/models/user.server";
+
+/**
+ * This base hook is used in other hooks to qu
