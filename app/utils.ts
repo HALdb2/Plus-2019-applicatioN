10f964a9ae12ddf826f1b9f47a60c0c6966b6d6c
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-08-05 12:00:07 +0000

    not used anymore

--- app/utils.ts
+++ app/utils.ts
@@ -7,4 +7,7 @@ import type { User } from "~/models/user.server";
  * This base hook is used in other hooks to quickly search for specific data
  * across all loader data using useMatches.
  * @param {string} id The route id
- * @returns {JSON|undefined} The router data or undefi
+ * @returns {JSON|undefined} The router data or undefined if not found
+ */
+export function useMatchesData(
+  id: st
