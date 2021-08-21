4fb5de76714b3bd344b05e6440e2cac08a3668fb
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-08-21 07:12:48 +0000

    fix build on windows

--- app/utils.ts
+++ app/utils.ts
@@ -36,4 +36,7 @@ export function useUser(): User {
   const maybeUser = useOptionalUser();
   if (!maybeUser) {
     throw new Error(
-      "No user found in root loader, but user is required by useUser. If user is opti
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead."
+    );
+  }
+  retur
