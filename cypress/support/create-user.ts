7aed20d8da3e58ac3c85733345df985dd3532e9e
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-09-09 08:43:26 +0000

    more UI enhancements

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,3 +1,4 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
-// node --require esbuild-register ./cypress/support/cr
+// node --require esbuild-register ./cypress/support/create-user.ts username@example.com
+// and it will l
