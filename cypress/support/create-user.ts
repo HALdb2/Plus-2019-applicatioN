4d9b05dd8c06a48e86ef1b5f9715a46c3d764cc2
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-09-09 09:56:23 +0000

    addressed review comments

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,4 +1,5 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
 // node --require esbuild-register ./cypress/support/create-user.ts username@example.com
-// and it will l
+// and it will log out the cookie value you can use to interact with the server
+//
