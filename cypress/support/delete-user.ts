d3c97d26084aa57ca68bfaa04d13a22b0dfabe40
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-09-26 14:31:41 +0000

    Upgrade typescript

--- cypress/support/delete-user.ts
+++ cypress/support/delete-user.ts
@@ -1,3 +1,4 @@
 // Use this to delete a user by their email
 // Simply call this with:
-// node --require esbuild-register ./cypress/support/delete
+// node --require esbuild-register ./cypress/support/delete-user.ts username@example.com
+// and that user will get dele
