2c674f18569c8860ae8fa388449359b31f4ebf2a
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2021-10-01 15:36:40 +0000

    Better .gitignore

--- cypress/support/delete-user.ts
+++ cypress/support/delete-user.ts
@@ -1,4 +1,7 @@
 // Use this to delete a user by their email
 // Simply call this with:
 // node --require esbuild-register ./cypress/support/delete-user.ts username@example.com
-// and that user will get dele
+// and that user will get deleted
+
+import { installGlobals } from "@remix-run/node/globals";
+import
