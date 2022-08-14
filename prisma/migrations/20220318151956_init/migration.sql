80c2f917e42604da510c597963af3f15ab03b598
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2022-08-14 16:55:43 +0000

    Add missing const

--- prisma/migrations/20220318151956_init/migration.sql
+++ prisma/migrations/20220318151956_init/migration.sql
@@ -3,4 +3,6 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "up
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIM
