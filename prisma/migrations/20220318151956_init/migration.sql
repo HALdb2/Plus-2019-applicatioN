b211fbc7e71c3d23a8dfb90d961935b1d00e63cd
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2022-10-01 21:09:23 +0000

    - MFT

--- prisma/migrations/20220318151956_init/migration.sql
+++ prisma/migrations/20220318151956_init/migration.sql
@@ -33,4 +33,4 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" F
+ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCAD
