a860c7210a4fbf06001ecffc2f9f0e769f263f94
HALdb2 <110372165+HALdb2@users.noreply.github.com>
2023-03-04 10:29:12 +0000

    added codeclimate badge

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,6 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vi
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineCo
